9da34b4b2238003becc536c0c3535738d83fb6f4
Hana Vavrušková <hana.vavruskova@gmail.com>
2023-11-11 22:49:34 +0100

    Úprava názvů pages, vytvoření nové page a reflexe všech změn v routeru

---
+++ src/pages/Mentors/index.jsx
@@ -0,0 +1,13 @@
+export const Mentors = () => {
+	return (
+		<div className="container">
+			<h2>Mentors</h2>
+			<p>
+				Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos
+				obcaecati dicta possimus inventore temporibus magnam voluptatibus
+				dolores vitae voluptates, facere quis et tempora provident, cupiditate
+				nesciunt impedit nihil fugit deserunt!
+			</p>
+		</div>
+	);
+};
